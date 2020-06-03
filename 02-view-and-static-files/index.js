8f06927ea13eba01e60dc670df8c095ae129f5fb
kunxin-chor <chorkunxin@yahoo.com>
2020-06-03 07:38:45 +0000

    Added new solutions

--- 02-view-and-static-files/index.js
+++ 02-view-and-static-files/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 // 1. Create the app
 var app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'hbs')
+
+
 // 2. Routes
 /** YOUR ROUTES HERE **/
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
 
 
 /** NO ROUTES AFTERWARDS */
