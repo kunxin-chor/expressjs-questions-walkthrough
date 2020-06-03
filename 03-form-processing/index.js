4b86662ff0b3424a4727e5082923cdfcdca2dc92
kunxin-chor <chorkunxin@yahoo.com>
2020-06-03 07:55:45 +0000

    Added form processing

--- 03-form-processing/index.js
+++ 03-form-processing/index.js
@@ -25,6 +25,16 @@ app.get('/about', (req,res)=>{
 
 app.get('/feedback', (req,res)=>res.render('feedback'))
 
+app.post('/feedback', (req,res)=>{
+    let name = req.body['user-name'];
+    let found_by = req.body['found-by'];
+    let helpful_sections = Array.isArray(req.body.helpful) ? req.body.helpful : [req.body.helpful];
+
+    res.render('results', {
+        name, found_by, helpful_sections
+    })
+})
+
 /** NO ROUTES AFTERWARDS */
 
 // 4. enable express
